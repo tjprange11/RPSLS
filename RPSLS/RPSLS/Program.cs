2e6aed7aa821ecc34d996ad4a3001cb4c21c6837
TJ Prange <tjprange11@gmail.com>
2018-03-23 08:28:06 -0500

    validate function

--- RPSLS/RPSLS/Program.cs
+++ RPSLS/RPSLS/Program.cs
@@ -10,6 +10,23 @@ namespace RPSLS
     {
         static void Main(string[] args)
         {
+            List<string> choices = new List<string>() { "rock", "paper", "scissors", "lizard", "spock"};
+        }
+        public string AskForChoice()
+        {
+            Console.WriteLine("Please choose : 'rock' , 'paper' , 'scissors' , 'lizard' , or 'spock'");
+            return Console.ReadLine().ToLower();
+        }
+        public bool validateChoice(string userInput, List<string> choices)
+        {
+            foreach(string choice in choices)
+            {
+                if(userInput.Equals(choice))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
